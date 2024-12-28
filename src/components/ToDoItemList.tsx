37fb45debec1f3f143cb0ff683e447226b2086ba
Jukka Ahonen <jukka.samuel@gmail.com>
2024-12-28 16:40:00 +0200

    ToDoItemList: render newItem

--- src/components/ToDoItemList.tsx
+++ src/components/ToDoItemList.tsx
@@ -3,13 +3,15 @@ import { ToDoItem, ToDoItemProps } from "./ToDoItem";
 
 type Props = {
   items: ToDoItemProps[];
+  newItem: ToDoItemProps | null;
 };
-export const ToDoItemList = ({ items }: Props) => {
+export const ToDoItemList = ({ items, newItem }: Props) => {
   return (
     <div className="to-do-item-list">
       {items.map((item) => (
         <ToDoItem {...item} />
       ))}
+      {newItem && <ToDoItem {...newItem} />}
       <AddItemButton />
     </div>
   );
